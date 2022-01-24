dde46a1a88e134130816b86984e4dc286ad15e5d
Dagi <dagi.zewdu.dz@gmail.com>
2022-01-24 19:37:37 +0300

    Add a hide effect to the mobile menu links

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mobileMenu = document.getElementById('mobile_menu');
 const closeButton = document.getElementById('close_button');
 const openButton = document.querySelector('.bt');
-
+const links= document.querySelectorAll('.mobile-menu li > a')
 function openOverlay(e) {
   mobileMenu.style.display = 'block';
 }
@@ -10,3 +10,4 @@ function closeOverlay(e) {
 }
 openButton.addEventListener('click', openOverlay);
 closeButton.addEventListener('click', closeOverlay);
+links.forEach(node=> node.addEventListener('click',closeOverlay))
