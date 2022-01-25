7365f56caf8729fa725fca71f7ec9aa76eef1fcd
vikipretium <vighneshpasupathy@gmail.com>
2022-01-25 13:21:22 +0530

    Add mobile menu and fix linters

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const mobileMenu = document.getElementById('mobile_menu');
-const closeButton = document.getElementById('close_button');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeButton = document.getElementById('close-button');
 const openButton = document.querySelector('.bt');
-const links= document.querySelectorAll('.mobile-menu li > a')
-function openOverlay(e) {
+const links = document.querySelectorAll('.mobile-menu li > a');
+function openOverlay() {
   mobileMenu.style.display = 'block';
 }
-function closeOverlay(e) {
+function closeOverlay() {
   mobileMenu.style.display = 'none';
 }
 openButton.addEventListener('click', openOverlay);
 closeButton.addEventListener('click', closeOverlay);
-links.forEach(node=> node.addEventListener('click',closeOverlay))
+links.forEach((node) => node.addEventListener('click', closeOverlay));
